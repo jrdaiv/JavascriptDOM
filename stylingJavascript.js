548dd25181277e8d9a2a119602992fdf20e29f56
jrdaiv <jrdaiv@gmail.com>
2024-07-30 19:07:49 -0700

    Number 3

---
+++ stylingJavascript.js
@@ -0,0 +1,36 @@
+const changeColor = () => {
+    const color1 = document.getElementById('name');
+    const color2 = document.getElementById('email');
+    const color3 = document.getElementById('message');
+    color1.style.backgroundColor = 'grey'
+    color2.style.backgroundColor = 'grey'
+    color3.style.backgroundColor = 'grey'
+
+}
+
+
+const changeColor2 = () => {
+    const color1 = document.getElementById('name')
+    const color2 = document.getElementById('email');
+    const color3 = document.getElementById('message');
+    color1.style.backgroundColor = 'white'
+    color2.style.backgroundColor = 'white'
+    color3.style.backgroundColor = 'white'
+}
+
+const btnColor = () => {
+    document.getElementById('myBtn').style.backgroundColor = 'grey';
+
+
+}
+
+
+const btnColor2 = () => {
+    document.getElementById('myBtn2').style.backgroundColor = 'grey';
+
+}
+
+
+
+
+
